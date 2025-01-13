c05001e9b3918514a123bca7100d628795aded56
hamzalamin <hamzalamin80@gmail.com>
2025-01-13 09:40:57 +0100

    Initial project setup

---
+++ src/main/java/com/wora/qatrat7ayat/Qatrat7ayatApplication.java
@@ -0,0 +1,13 @@
+package com.wora.qatrat7ayat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qatrat7ayatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Qatrat7ayatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wora/qatrat7ayat/Qatrat7ayatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wora.qatrat7ayat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qatrat7ayatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
