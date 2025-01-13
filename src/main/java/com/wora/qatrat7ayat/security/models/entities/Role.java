e877bfaec776aa87181365ce7cb35641dfa2f63a
hamzalamin <hamzalamin80@gmail.com>
2025-01-13 10:21:40 +0100

    feat: add User and Role entities with relationships
    - Created User entity with fields for personal information and role association
    - Implemented Role entity with a one-to-many relationship to users
    - Added validation annotations for input validation
    - Set up JPA relationships with proper mappings between User and Role entities

--- src/main/java/com/wora/qatrat7ayat/security/models/entities/Role.java
+++ src/main/java/com/wora/qatrat7ayat/security/models/entities/Role.java
@@ -1,4 +1,29 @@
 package com.wora.qatrat7ayat.security.models.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<User> users;
 }
--- src/main/java/com/wora/qatrat7ayat/security/models/entities/User.java
+++ src/main/java/com/wora/qatrat7ayat/security/models/entities/User.java
@@ -1,4 +1,63 @@
 package com.wora.qatrat7ayat.security.models.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NotBlank
+    @Column(name = "last_name")
+    private String lastName;
+
+    @NotBlank
+    @Column(name = "psudo_name")
+    private String psudoName;
+
+    @NotBlank
+    @Column(name = "phone")
+    private String phone;
+
+    @NotNull
+    @Column(name = "blood_type")
+    private String bloodType;
+
+    @NotBlank
+    @Column(name = "city")
+    private String city;
+
+    @NotBlank
+    @Column(name = "availability_message")
+    private String availabilityMessage;
+
+    @NotBlank
+    @Column(name = "created_at")
+    private String createdAt;
+
+    @NotBlank
+    @Column(name = "updated_at")
+    private String updatedAt;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
 }
