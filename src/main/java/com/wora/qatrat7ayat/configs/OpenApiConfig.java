e1abac760978a6c6ac9adf5daff610bfa35904c8
hamzalamin <hamzalamin80@gmail.com>
2025-01-20 16:03:04 +0100

    chore: configure Swagger for API documentation
    - Added Swagger configuration for generating API documentation
    - Enabled detailed endpoint descriptions and request/response schemas
    - Integrated Swagger UI for interactive API exploration

---
+++ src/main/java/com/wora/qatrat7ayat/configs/OpenApiConfig.java
@@ -0,0 +1,36 @@
+package com.wora.qatrat7ayat.configs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI defineApi(){
+        Server server = new Server();
+        server.setUrl("http://localhost:8080");
+        server.setDescription("Development server");
+
+        Contact contact = new Contact();
+        contact.setName("Hamza LAMIN");
+        contact.setEmail("hamzalamin80@gmail.com");
+        contact.setUrl("https://www.linkedin.com/in/hamza-lamin-a0440a296/");
+
+        Info info = new Info()
+                .title("API REST Qatrat Hayat")
+                .version("1.0")
+                .description("Plateforme web de gestion des dons de sang permettant de connecter donneurs, " +
+                        "demandeurs et entreprises soutenant les initiatives solidaires. Suivi des besoins, " +
+                        "cartographie des centres, et gestion simplifiée des profils pour sauver des vies")
+                .contact(contact);
+        return new OpenAPI().info(info).servers(List.of(server));
+    }
+
+}
