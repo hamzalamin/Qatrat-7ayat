2fb1dcdbf3c66630aa6f7599049ccb7883347b5f
hamzalamin <hamzalamin80@gmail.com>
2025-02-06 09:28:53 +0100

    add seeders for the Hospital

---
+++ src/main/java/com/wora/qatrat7ayat/repositories/HospitalRepository.java
@@ -0,0 +1,7 @@
+package com.wora.qatrat7ayat.repositories;
+
+import com.wora.qatrat7ayat.models.entities.Hospital;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+}
