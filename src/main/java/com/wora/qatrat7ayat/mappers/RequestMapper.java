cd80123d5d2215114e90669a2bc8e9d2ca5b3c23
hamzalamin <hamzalamin80@gmail.com>
2025-02-16 12:24:44 +0100

    add : request mapper utils

--- src/main/java/com/wora/qatrat7ayat/mappers/RequestMapper.java
+++ src/main/java/com/wora/qatrat7ayat/mappers/RequestMapper.java
@@ -8,15 +8,17 @@ import com.wora.qatrat7ayat.models.DTOs.action.request.UpdateRequestDto;
 import com.wora.qatrat7ayat.models.entities.Request;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = RequestMapperUtils.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RequestMapper extends GenericMapper<Request, RequestDto> {
+
     Request toEntity(RequestDto dto);
     Request toEntity(UpdateRequestDto dto);
     Request toEntity(CreateRequestDto dto);
     Request toEntity(EmbeddedRequestDto dto);
-    @Mapping(source = "hospital.id", target = "request.hospitalId")
+
     @Mapping(target = "profile", source = "user")
-    @Mapping(target = "request", source = ".")
+    @Mapping(target = "request", source = "request", qualifiedByName = "toEmbeddedRequestDto")
     RequestDto toDto(Request request);
 }
--- src/main/java/com/wora/qatrat7ayat/models/DTOs/action/request/EmbeddedRequestDto.java
+++ src/main/java/com/wora/qatrat7ayat/models/DTOs/action/request/EmbeddedRequestDto.java
@@ -1,7 +1,6 @@
 package com.wora.qatrat7ayat.models.DTOs.action.request;
 
 import com.wora.qatrat7ayat.models.DTOs.action.CreateActionDto;
-import com.wora.qatrat7ayat.models.enumes.BloodType;
 import com.wora.qatrat7ayat.models.enumes.UrgencyLevel;
 import lombok.Getter;
 
@@ -10,7 +9,7 @@ public class EmbeddedRequestDto extends CreateActionDto {
     private final Float bloodVolume;
     private final UrgencyLevel urgencyLevel;
 
-    public EmbeddedRequestDto(String message, Long userId, Long hospitalId, String firstName, String lastName, String psudoName, String phone, BloodType bloodType, Long city, String message1, Long hospital, Long user, Float bloodVolume, UrgencyLevel urgencyLevel) {
+    public EmbeddedRequestDto(String message, Long userId, Long hospitalId, Float bloodVolume, UrgencyLevel urgencyLevel) {
         super(message, userId, hospitalId);
         this.bloodVolume = bloodVolume;
         this.urgencyLevel = urgencyLevel;
