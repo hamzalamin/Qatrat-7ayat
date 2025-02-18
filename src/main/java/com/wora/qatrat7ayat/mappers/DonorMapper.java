801d95071c575373c3bcf45add9b7fcf196e9139
hamzalamin <hamzalamin80@gmail.com>
2025-02-18 14:37:52 +0100

    create the mapper of donor

--- src/main/java/com/wora/qatrat7ayat/mappers/DonorMapper.java
+++ src/main/java/com/wora/qatrat7ayat/mappers/DonorMapper.java
@@ -3,14 +3,19 @@ package com.wora.qatrat7ayat.mappers;
 import com.wora.qatrat7ayat.mappers.api.GenericMapper;
 import com.wora.qatrat7ayat.models.DTOs.action.donor.CreateDonorDto;
 import com.wora.qatrat7ayat.models.DTOs.action.donor.DonorDto;
+import com.wora.qatrat7ayat.models.DTOs.action.donor.EmbeddedDonorDto;
 import com.wora.qatrat7ayat.models.DTOs.action.donor.UpdateDonorDto;
 import com.wora.qatrat7ayat.models.entities.Donor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-//@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring")
 public interface DonorMapper extends GenericMapper<Donor, DonorDto> {
     Donor toEntity(DonorDto dto);
-    Donor toEntity(CreateDonorDto dto);
     Donor toEntity(UpdateDonorDto dto);
-    DonorDto toDto(Donor action);
+    Donor toEntity(CreateDonorDto dto);
+    Donor toEntity(EmbeddedDonorDto dto);
+    @Mapping(target = "profile", source = "user")
+    @Mapping(target = "donor", source = "donor")
+    DonorDto toDto(Donor donor);
 }
